c7f2101ca55a5d060a4f0b9b2d6ab532f16d5cc8
michauciak <michauciak@gmail.com>
2017-01-22 17:23:49 +0100

    Adding file with BoardData class.

---
+++ src/main/java/entrants/BoardData.java
@@ -0,0 +1,396 @@
+package entrants;
+
+import java.util.EnumMap;
+
+import pacman.game.Constants.GHOST;
+import pacman.game.comms.BasicMessage;
+import pacman.game.comms.Message;
+import pacman.game.comms.Message.MessageType;
+import pacman.game.comms.Messenger;
+import pacman.game.Game;
+import pacman.game.internal.Maze;
+import pacman.game.internal.Node;
+
+
+/** Co najmniej te metody chcemy udostępniać. */
+interface IBoardData {
+	public void update(Game game);
+	public String toString();
+	
+	public boolean isWall(int x, int y);
+	public boolean isWall(int index);
+	public boolean isEmpty(int x, int y);
+	public boolean isEmpty(int index);
+	public boolean isPill(int x, int y);
+	public boolean isPill(int index);
+	public boolean isPowerPill(int x, int y);
+	public boolean isPowerPill(int index);
+	public boolean isPacman(int x, int y, int treshold);
+	public boolean isPacman(int index, int treshold);
+	public boolean isGhost(GHOST ghost, int x, int y, int treshold);
+	public boolean isGhost(GHOST ghost, int index, int treshold);
+	
+	public void setPacmanIndex(int index);
+	public void setPacmanIndex(int index, int time);
+	public DataTime getPacmanIndex();
+	public void setGhostIndex(GHOST ghost, int index);
+	public void setGhostIndex(GHOST ghost, int index, int time);
+	public DataTime getGhostIndex(GHOST ghost);	
+}
+
+
+/** Instancja klasy BoardData powinna być zmienną instancji kontrolera 
+ *  danego duszka / pacmana. Kontruktor bez parametrów jest odpowiedni dla 
+ *  kontrolera pacmana, dla kontrolera duszka trzeba wywołać go z nazwą duszka
+ *  i flagą mówiącą czy chcemy użyć wysyłania i odbierania wiadomości o zobaczeniu
+ *  pacmana i pozycjach duszków. Na początku każdego przebiegu funkcji getMove
+ *  w kontrolerze należy wywołać metodę update(Game game) obiektu klasy BoardData.
+ *  Można sobie wyprintować obiekt BoardData (planszę i inne dane) dzięki toString(). */
+public class BoardData implements IBoardData {
+	private int height = 120;
+	private int width = 109;
+	private char[][] board = new char[height][width];
+	private char wallChar = '.';
+	private char pillChar = '*';
+	private char powerPillChar = '$';
+	private char corridorChar = ' ';
+	private char pacmanChar = '@';
+	
+	private Game game;
+	private Node[] nodes;
+	private Maze maze;
+	private int[] pillIndices;
+	private int[] powerPillIndices;
+	private int level = -1;
+	private final int pacmanInitIndex = 978;
+	
+	private DataTime pacmanIndex;
+	private EnumMap<GHOST, DataTime> ghostIndices = new EnumMap<>(GHOST.class);
+
+	private boolean messaging = false;
+	private final GHOST clientGhost;
+	
+	
+	/** Konstruktor dla pacmana. */
+	public BoardData() {
+		this.clientGhost = null;
+	}
+	/** Konstruktor dla duszka. Flaga messaging odpowiada za włączenie 
+	 *  wysyłania i odbierania wiadomości o zobaczeniu pacmana i pozycji duszka. */
+	public BoardData(GHOST clientGhost, boolean messaging) {
+		this.messaging = messaging;
+		this.clientGhost = clientGhost;
+	}
+	
+	/** Inicjuje planszę i informacje, gdy rozpoczyna się nowy poziom. */
+	private void initBoard() {
+		level = game.getCurrentLevel();
+		
+		// na początek wszystko jest ścianą
+		for (int x = 0; x < width; ++x) {
+			for (int y = 0; y < height; ++y) {
+				setBoardElement(x, y, wallChar);
+			}
+		}
+		
+		// budujemy korytarze
+    	maze = game.getCurrentMaze();
+    	nodes = maze.graph;
+		for (Node node : nodes) {
+			setBoardElement(node.x, node.y, corridorChar);
+		}
+		
+		// dostawiamy tabletki
+		pillIndices = maze.pillIndices;
+		for (int pillIndex : pillIndices) {
+			setBoardElement(pillIndex, pillChar);
+		}
+		
+		// dostawiamy potężne tabletki
+		powerPillIndices = maze.powerPillIndices;
+		for (int powerPillIndex : powerPillIndices) {
+			setBoardElement(powerPillIndex, powerPillChar);
+		}
+		
+		// inicjacja początkowymi pozycjami pacmana i duchów
+		initPositions();
+	}
+	
+	/** Aktualizacja stanów. Metoda powinna być wywołana na początku
+	 *  każdego przebiegu funkcji getMove. */
+	public void update(Game game) {
+		this.game = game;
+		if (level < game.getCurrentLevel()) {
+			initBoard();
+		}
+    	
+		updatePills();
+		updatePowerPills();
+		updatePacman();
+		updateGhosts();
+		if (game.wasPacManEaten()) {
+			initPositions();
+		}
+		if (messaging) {
+			getMessages();
+		}
+	}
+	
+	private void updatePills() {
+		for (int pillIndex : pillIndices) {
+			if (isPill(pillIndex) &&
+					game.isNodeObservable(pillIndex) &&
+					!game.isPillStillAvailable(game.getPillIndex(pillIndex))) {
+				clearElement(pillIndex);
+			}
+		}
+	}
+	
+	private void updatePowerPills() {
+		for (int powerPillIndex : powerPillIndices) {
+			if (isPowerPill(powerPillIndex) &&
+					game.isNodeObservable(powerPillIndex) &&
+					!game.isPowerPillStillAvailable(game.getPowerPillIndex(powerPillIndex))) {
+				clearElement(powerPillIndex);
+			}
+		}
+	}
+	
+	private void updatePacman() {
+		int pacmanCurrentIndex = game.getPacmanCurrentNodeIndex();
+		if (pacmanCurrentIndex >= 0) {
+			setPacmanIndex(pacmanCurrentIndex);
+			if (messaging) {
+				sendMessagePacmanSeen(pacmanCurrentIndex);
+			}
+		}
+	}
+	
+	private void updateGhosts() {
+		for (GHOST ghost : GHOST.values()) {
+			int ghostCurrentIndex = game.getGhostCurrentNodeIndex(ghost);
+			if (ghostCurrentIndex >= 0) {
+				setGhostIndex(ghost, ghostCurrentIndex);
+				if (messaging && ghost == clientGhost) {
+					sendMessageIAm(ghostCurrentIndex);
+				}
+			}
+		}
+	}
+	
+	private void sendMessagePacmanSeen(int index) {
+		broadcastMessage(MessageType.PACMAN_SEEN, index);
+//    	System.out.format("%d. %s wysyła info o pacmanie, że jest on w %d\n",
+//    			game.getCurrentLevelTime(), clientGhost, index); 
+	}
+	
+	private void sendMessageIAm(int index) {
+		broadcastMessage(MessageType.I_AM, index);
+//    	System.out.format("%d. %s wysyła info o sobie, że jest w %d\n",
+//    			game.getCurrentLevelTime(), clientGhost, index); 
+	}
+	
+	private void broadcastMessage(MessageType type, int data) {
+		Messenger messenger = game.getMessenger();
+		messenger.addMessage(
+				new BasicMessage(clientGhost, null, type, data, game.getCurrentLevelTime()));
+	}
+	
+	private void getMessages() {
+		Messenger messenger = game.getMessenger();
+        for (Message message : messenger.getMessages(clientGhost)) {
+            if (message.getType() == MessageType.PACMAN_SEEN) {
+//            	System.out.format("%d. %s odbiera od %s info o pacmanie podczas %d, posiadając dane z %d\n",
+//            			game.getCurrentLevelTime(), clientGhost, message.getSender(), message.getTick(), getPacmanIndex().time); 
+
+                if (message.getTick() > getPacmanIndex().time) {
+                	setPacmanIndex(message.getData(), message.getTick());
+                }
+            }
+            else if (message.getType() == MessageType.I_AM) {
+//            	System.out.format("%d. %s odbiera od %s obecność podczas %d, posiadając dane z %d\n",
+//                		game.getCurrentLevelTime(), clientGhost, message.getSender(), message.getTick(), getGhostIndex(message.getSender()).time); 
+                if (message.getTick() > getGhostIndex(message.getSender()).time) {
+                	setGhostIndex(message.getSender(), message.getData(), message.getTick());
+                }
+                			}
+        }		
+	}
+	
+	private void initPositions() {
+		setPacmanIndex(pacmanInitIndex);
+		for (GHOST ghost : GHOST.values()) {
+			setGhostIndex(ghost, game.getGhostInitialNodeIndex());
+		}
+	}
+
+	
+	
+	public boolean isWall(int x, int y) {
+		return compareBoardElement(x, y, wallChar);
+	}
+	
+	public boolean isWall(int index) {
+		return isWall(indexX(index), indexY(index));
+	}
+	
+	public boolean isEmpty(int x, int y) {
+		return compareBoardElement(x, y, corridorChar);
+	}
+	
+	public boolean isEmpty(int index) {
+		return isEmpty(indexX(index), indexY(index));
+	}
+	
+	public boolean isPill(int x, int y) {
+		return compareBoardElement(x, y, pillChar);
+	}
+	
+	public boolean isPill(int index) {
+		return isPill(indexX(index), indexY(index));
+	}
+	
+	public boolean isPowerPill(int x, int y) {
+		return compareBoardElement(x, y, powerPillChar);
+	}
+	
+	public boolean isPowerPill(int index) {
+		return isPowerPill(indexX(index), indexY(index));
+	}
+	
+	public boolean isPacman(int x, int y, int treshold) {
+		return compareIndexCoords(getPacmanIndex().value, x, y) && !expired(getPacmanIndex().time, treshold);
+	}
+	
+	public boolean isPacman(int index, int treshold) {
+		return index == getPacmanIndex().value && !expired(getPacmanIndex().time, treshold);
+	}
+
+	public boolean isGhost(GHOST ghost, int x, int y, int treshold) {
+		return compareIndexCoords(getGhostIndex(ghost).value, x, y) && !expired(getGhostIndex(ghost).time, treshold);
+	}
+	
+	public boolean isGhost(GHOST ghost, int index, int treshold) {
+		return index == getGhostIndex(ghost).value && !expired(getGhostIndex(ghost).time, treshold);
+	}
+	
+	/** Sprawdza, czy wydarzenie się przeterminowało. */
+	private boolean expired(int eventTime, int treshold) {
+		return game.getCurrentLevelTime() - eventTime > treshold;
+	}
+	
+	
+	
+	
+	
+	public void setPacmanIndex(int index) {
+		setPacmanIndex(index, game.getCurrentLevelTime());
+	}
+	
+	public void setPacmanIndex(int index, int time) {
+		pacmanIndex = new DataTime(index, time);
+	}
+	
+	public DataTime getPacmanIndex() {
+		return pacmanIndex;
+	}
+	
+	public void setGhostIndex(GHOST ghost, int index) {
+		setGhostIndex(ghost, index, game.getCurrentLevelTime());
+	}
+	
+	public void setGhostIndex(GHOST ghost, int index, int time) {
+		ghostIndices.put(ghost, new DataTime(index, time));
+	}
+	
+	public DataTime getGhostIndex(GHOST ghost) {
+		return ghostIndices.getOrDefault(ghost, null);
+	}
+	
+	private boolean compareIndexCoords(int index, int x, int y) {
+		return indexX(index) == x && indexY(index) == y;
+	}
+	
+	private void clearElement(int x, int y) {
+		setBoardElement(x, y, corridorChar);
+	}
+	
+	private void clearElement(int index) {
+		clearElement(indexX(index), indexY(index));
+	}
+	
+	private boolean compareBoardElement(int x, int y, char value) {
+		return getBoardElement(x, y) == value;
+	}
+	
+	private void setBoardElement(int x, int y, char value) {
+		board[y][x] = value;
+	}
+	
+	private void setBoardElement(int index, char value) {
+		setBoardElement(indexX(index), indexY(index), value);
+	}
+	
+	private char getBoardElement(int x, int y) {
+		return board[y][x];
+	}
+	
+	private char getBoardElement(int index) {
+		return getBoardElement(indexX(index), indexY(index));
+	}
+	
+	private int indexX(int index) {
+		return nodes[index].x;
+	}
+	
+	private int indexY(int index) {
+		return nodes[index].y;
+	}
+	
+	/** Zamienia w stringa informacje o grze i planszę. Duszki są oznaczone
+	 *  przez ich pierwsze litery, pacman i reszta planszy wg zmiennych wallChar itd. */
+	public String toString() {
+		// ogólne informacje
+		String str = String.format("CURRENT GAME KNOWLEDGE OF %s\nLevel: %d, level time: %d, pacman score: %d, pacman lives: %d\n",
+									(clientGhost == null ? "PACMAN" : clientGhost),
+									level,
+									game.getCurrentLevelTime(),
+									game.getScore(),
+									game.getPacmanNumberOfLivesRemaining());
+		str += "Localization data time: ";
+		for (GHOST ghost : GHOST.values()) {
+			str += String.format("%s %d, ", ghost, getGhostIndex(ghost).time);
+		}
+		str += String.format("pacman %d\n", getPacmanIndex().time);
+		
+		// wstawienie duszków i pacmana na planszę
+		char[][] boardCopy = new char[board.length][];
+		for (int i = 0; i < board.length; ++i) {
+			boardCopy[i] = board[i].clone();
+		}
+		setBoardElement(getPacmanIndex().value, pacmanChar);
+		for (GHOST ghost : GHOST.values()) {
+			setBoardElement(getGhostIndex(ghost).value, ghost.toString().charAt(0));
+		}
+		
+		// zbudowanie planszy
+		for (int i = 0; i < height; ++i) {
+			str += String.valueOf(board[i]) + '\n';
+		}
+		
+		// przywrócenie planszy bez duszków i pacmana
+		board = boardCopy;
+		return str;
+	}
+}
+
+
+/** Struktura do zapisania danych (indeksu na planszy) i czasu pozyskania danych. */
+class DataTime {
+	int value;
+	int time;
+	public DataTime(int value, int time) {
+		this.value = value;
+		this.time = time;
+	}
+}
